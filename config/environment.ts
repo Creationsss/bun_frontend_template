d6a25705b44735a0aa5aa406842907399a925ac5
creations <creations@creations.works>
2025-04-11 04:07:42 -0400

    move to biomejs instead of eslint

--- config/environment.ts
+++ config/environment.ts
@@ -1,7 +1,6 @@
 export const environment: Environment = {
-	port: parseInt(process.env.PORT || "8080", 10),
+	port: Number.parseInt(process.env.PORT || "8080", 10),
 	host: process.env.HOST || "0.0.0.0",
 	development:
-		process.env.NODE_ENV === "development" ||
-		process.argv.includes("--dev"),
+		process.env.NODE_ENV === "development" || process.argv.includes("--dev"),
 };
